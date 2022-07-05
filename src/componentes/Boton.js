fa60c62da2710252e4e7533b33fa3a8ccaf176f0
unknown <development.ven@gmail.com>
2022-07-05 15:18:48 -0400

    Componente Pantalla

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -12,7 +12,9 @@ function Boton(props){
         className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
           {props.children}
           {/* de esta manera podremos declarar los componentes con sin la etiqueta de autocierre usando la nomenclatura
-          por defecto de html con su etiqueta de aperta y cierre */}
+          por defecto de html con su etiqueta de aperta y cierre
+          Nota: Se hace de esta manera porque el proyecto necesita agregar texto en el mismo y esta nomenclatura lo facilitara
+          */}
         </div>
     );
 }
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+// Forma alternativa de definir componentes funcionales, no siempre usamos function
+// podemos unsar const y definir como flecha
+//Normalmente se usa cuando es un componente sencillo, sin logica ni funciones
+
+const Pantalla = ({ input }) => (
+<div className='input'>
+{input}
+</div>
+);
+
+export default Pantalla;
